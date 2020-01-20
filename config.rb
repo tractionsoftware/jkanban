bf0cf23d28677ea2fb64c6e2f7d96403508813d3
Takashi <takashi@electra.local>
2020-01-20 22:35:33 +0900

    Modified the jkanban.css to reduce the horizontal padding in the kanban-drag area.

---
+++ config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "dist"
+sass_dir = "/"
+images_dir = "images"
+javascripts_dir = "dist"
+#output_style = :compressed
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+preferred_syntax = :scss
+
+# include standard teampage sass
+#add_import_path "../../../../teampage/html/sass"
+
+sourcemap = false
+cache = false
